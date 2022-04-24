dab58971a8bc99401cf328502366e269d8311c1c
Avaer Kazmer <a@modules.io>
2022-04-24 07:46:46 -0400

    Rewrite to use mesh lodder

--- index.js
+++ index.js
@@ -1,44 +1,78 @@
 // import * as THREE from 'three';
 // import easing from './easing.js';
 import metaversefile from 'metaversefile';
-const {useApp, useLoaders, usePhysics, useCleanup} = metaversefile;
+const {useApp, useFrame, useLoaders, usePhysics, useMeshLodder, useCleanup} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
+const glbUrls = [
+  `${baseUrl}plants.glb`,
+  `${baseUrl}rocks.glb`,
+];
+
+/* const _makeLodArray = () => {
+  const lods = Array(3);
+  for (let i = 0; i < 3; i++) {
+    lods[i] = [];
+  };
+  return lods;
+}; */
 
 export default () => {
   const app = useApp();
   const physics = usePhysics();
+  const MeshLodder = useMeshLodder();
 
   app.name = 'plants';
 
+  const meshLodder = new MeshLodder();
+
+  const lods = {};
   let physicsIds = [];
   (async () => {
-    const u = `${baseUrl}plants.glb`;
-    let o = await new Promise((accept, reject) => {
-      const {gltfLoader} = useLoaders();
-      gltfLoader.load(u, accept, function onprogress() {}, reject);
-    });
-    o = o.scene;
+    await Promise.all(glbUrls.map(async glbUrl => {
+      const u = glbUrl;
+      let o = await new Promise((accept, reject) => {
+        const {gltfLoader} = useLoaders();
+        gltfLoader.load(u, accept, function onprogress() {}, reject);
+      });
+      o = o.scene;
+      o.updateMatrixWorld();
 
-    const lods = Array(3);
-    for (let i = 0; i < 3; i++) {
-      lods[i] = [];
-    }
-    o.traverse(o => {
-      if (o.isMesh) {
-        const match = o.name.match(/LOD([012])/);
-        if (match) {
-          const index = parseInt(match[1], 10);
-          lods[index].push(o);
+      o.traverse(o => {
+        if (o.isMesh) {
+          const match = o.name.match(/^(.+)_LOD([012])$/);
+          if (match) {
+            const name = match[1];
+            const index = parseInt(match[2], 10);
+
+            let ls = lods[name];
+            if (!ls) {
+              ls = Array(3).fill(null);
+              lods[name] = ls;
+            }
+            ls[index] = o;
+          }
         }
-      }
-    });
+      });
 
-    console.log('got plants', lods);
+      // app.add(o);
+    }));
 
-    app.add(o);
-    o.updateMatrixWorld();
+    // console.log('got lods', lods);
+    for (const name in lods) {
+      const ls = lods[name];
+      meshLodder.registerLodMesh(name, ls);
+    }
+    meshLodder.compile();
   })();
+
+  const chunksObject = meshLodder.getChunks();
+  app.add(chunksObject);
+  chunksObject.updateMatrixWorld();
+
+  useFrame(() => {
+    meshLodder.update();
+  });
   
   useCleanup(() => {
     for (const physicsId of physicsIds) {
