ff0ccaa47d86f834835f21f5d1d7f173ab39ebec
Avaer Kazmer <a@modules.io>
2022-05-06 02:48:17 -0400

    Major refactoring to use mesh lod item metaverse module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useApp, useFrame, useActivate, useLoaders, usePhysics, useMeshLodder, useCleanup} = metaversefile;
+const {useApp, useFrame, useActivate, useLoaders, usePhysics, useMeshLodder, useCleanup, useWorld, useDefaultModules, createApp} = metaversefile;
+// import * as metaverseModules from './metaverse-modules.js';
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 const glbUrls = [
@@ -8,10 +9,17 @@ const glbUrls = [
   `${baseUrl}rocks.glb`,
 ];
 
+const localVector = new THREE.Vector3();
+const localVector2 = new THREE.Vector3();
+const localQuaternion = new THREE.Quaternion();
+const localMatrix = new THREE.Matrix4();
+
 export default () => {
   const app = useApp();
+  const world = useWorld();
   const physics = usePhysics();
   const meshLodManager = useMeshLodder();
+  // const scene = useScene();
 
   app.name = 'plants';
 
@@ -72,10 +80,75 @@ export default () => {
 
   app.getPhysicsObjects = () => meshLodder.getPhysicsObjects();
 
+  const _loadMeshLodApp = async ({
+    position,
+    quaternion,
+    scale,
+    meshLodderId,
+    physicsId,
+  }) => {
+    const app = await world.appManager.addTrackedApp(
+      `./metaverse_modules/mesh-lod-item/index.js`,
+      position,
+      quaternion,
+      scale,
+      [
+        {
+          key: 'meshLodderId',
+          value: meshLodderId,
+        },
+        {
+          key: 'physicsId',
+          value: physicsId,
+        },
+      ],
+    );
+    return app;
+  };
+
   useActivate(e => {
     const item = meshLodder.getItemByPhysicsId(e.physicsId);
-    const itemMesh = item.cloneApp();
-    meshLodder.deleteItem(item);
+    // const {position, quaternion, scale} = item;
+    localMatrix.compose(item.position, item.quaternion, item.scale)
+      .premultiply(app.matrixWorld)
+      .decompose(localVector, localQuaternion, localVector2);
+    const position = localVector;
+    const quaternion = localQuaternion;
+    const scale = localVector2;
+
+    (async () => {
+      const meshLodItemApp = await _loadMeshLodApp({
+        position,
+        quaternion,
+        scale,
+        meshLodderId: meshLodder.id,
+        physicsId: e.physicsId,
+      });
+      console.log('mesh lod item app', meshLodItemApp, {
+        position: position.toArray().join(','),
+        quaternion: quaternion.toArray().join(','),
+        scale: scale.toArray().join(','),
+      });
+
+      meshLodder.deleteItem(item);
+    })();
+    /* {
+      // XXX this should be a tracked app
+      const meshLodItemApp = createApp();
+      meshLodItemApp.setComponents({
+        meshLodderId: meshLodder.id,
+        physicsId: e.physicsId,
+      });
+      (async () => {
+        const {modules} = useDefaultModules();
+        const m = modules['meshLodItem'];
+        await meshLodItemApp.addModule(m);
+
+        app.add(meshLodItemApp);
+
+        meshLodder.deleteItem(item);
+      })();
+    } */
   });
 
   useFrame(() => {
